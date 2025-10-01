66aa9ec9a9ee1b8fb8a6fb8560283930f41d0ed5
emancilla01 <emancilla01@gmail.com>
2025-09-30 22:48:43 -0500

    uncommented dashboard

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::get('/', function () {
 
 Route::get('dashboard', function () {
     return view("inicio_autententicado"); //Inertia::render('Dashboard');
-})->middleware(['auth', 'verified']); //->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
