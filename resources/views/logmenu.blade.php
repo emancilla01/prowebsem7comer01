9614f49b380e21bc1290c0859261a34887b96855
emancilla01 <emancilla01@gmail.com>
2025-10-08 16:25:12 -0500

    modified

--- resources/views/logmenu.blade.php
+++ resources/views/logmenu.blade.php
@@ -2,15 +2,7 @@
     class="navbar navbar-expand-sm navbar-dark bg-primary"
    >
     <a class="navbar-brand" href="#">Comercializadora</a>
-    {{-- <button
-        class="navbar-toggler d-lg-none"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#collapsibleNavId"
-        aria-controls="collapsibleNavId"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-    ></button> --}}
+    
     <div class="collapse navbar-collapse" id="collapsibleNavId">
         <ul class="navbar-nav me-auto mt-2 mt-lg-0">
             <li class="nav-item">
